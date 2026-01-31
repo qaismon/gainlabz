030fd3bc18ebb5215a21b6ea0475eab72da2fcdc
qaismon <qaismon127@gmail.com>
2026-01-31 10:17:27 +0530

    changed env var name

--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -60,7 +60,7 @@ function PlaceOrder() {
         const orderItems = getCartItemsArray();
 
         const options = {
-            key: import.meta.env.VITE_RAZORPAY_KEY_ID, 
+            key: import.meta.env.RAZORPAY_KEY_ID, 
             amount: orderData.amount,
             currency: orderData.currency,
             name: "GymFluence",
